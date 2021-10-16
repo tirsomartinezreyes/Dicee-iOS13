17ae3df29c97868fc1cc39f4dc84d9913d14ec03
tirsomartinezreyes <tirsomartinezreyes@gmail.com>
2021-10-16 11:43:17 -0500

    proyecto funcionando

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //TMR: links (References Outlets) made in Main.storyboaard using GUI and assistan code view, in case of rename use "refactor->rename" to avoid broken references
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    
+    //TMR Action linked in Main.storyboard GUI and assistant code view (Touch UP inside)
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        if(dices.count > 0){
+            diceImageView1.image = dices[ Int.random(in: 0..<dices.count) ]
+            diceImageView2.image = dices[ Int.random(in: 0..<dices.count) ]
+        }
     }
-
-
+    
 }
 
